3cc11488b258a47bfb4f1e9d7827e4db6548bd92
grupoprolider <cobranzas1@grupoprolider.com.ar>
2026-09-08 13:32:01 -0300

    Delete change_color.js

--- change_color.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-let html = fs.readFileSync('seguimiento.html', 'utf8');
-
-// The line is: let darkBg = '#004d5a'; // Match the user's header/total color
-html = html.replace("let darkBg = '#004d5a';", "let darkBg = '#A42A68';");
-
-fs.writeFileSync('seguimiento.html', html);
-console.log("Color updated!");
